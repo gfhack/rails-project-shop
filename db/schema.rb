f25209c6901644afbf178933a737abbc656bba67
Guilherme Freire Hack <gfreirehack@gmail.com>
2019-06-15 17:23:55 -0300

    creates product, uploads image and uses bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190615190807) do
+ActiveRecord::Schema.define(version: 20190615200337) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "avatar"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
