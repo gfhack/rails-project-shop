1061e3800196f3ac1e1d07bbaf98ba29ec0009fd
Guilherme Freire Hack <gfreirehack@gmail.com>
2019-06-18 13:14:19 -0300

    create product list

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
+  validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190618002156) do
+ActiveRecord::Schema.define(version: 20190618154436) do
+
+  create_table "items", force: :cascade do |t|
+    t.integer  "list_id"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "items", ["list_id"], name: "index_items_on_list_id"
+  add_index "items", ["product_id"], name: "index_items_on_product_id"
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "bought"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
